e71ebd5add9679573174888d4e5cb520b128d734
Ardeman <are.theman@gmail.com>
2024-09-23 07:46:20 +0800

    Refactor blog component to update section ID and heading text

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -1,13 +1,13 @@
 export const Blog = () => {
   return (
     <section
-      id="writing"
+      id="blog"
       className="scroll-mt-16 lg:scroll-mt-24"
       aria-label="Blog posts"
     >
       <div className="bg-secondary/75 sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-primary text-sm font-bold uppercase tracking-widest lg:sr-only">
-          Writing
+          Blog
         </h2>
       </div>
       <div>
