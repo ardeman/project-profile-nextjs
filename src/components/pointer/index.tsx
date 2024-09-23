0853826a035a8c07793388449f21b09af43a7c01
Ardeman <are.theman@gmail.com>
2024-09-23 19:02:54 +0800

    Refactor CSS to include pointer color variable

--- src/components/pointer/index.tsx
+++ src/components/pointer/index.tsx
@@ -7,7 +7,7 @@ export const Pointer = (props: TProps) => {
     <div
       className="pointer-events-none fixed inset-0 z-30 cursor-none transition duration-300"
       style={{
-        background: `radial-gradient(600px at ${position.x}px ${position.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+        background: `radial-gradient(600px at ${position.x}px ${position.y}px, var(--pointer), transparent 80%)`,
       }}
     />
   )
