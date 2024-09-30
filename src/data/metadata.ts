502041ec5f1d4d8cb162319f7f935bffba91bb79
Ardeman <are.theman@gmail.com>
2024-09-30 18:38:39 +0800

    Add favicon and site manifest files for dark and light themes

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -4,4 +4,18 @@ export const metadata: Metadata = {
   title: 'Ardeman',
   description:
     'Ardeman is a software engineer dedicated to creating accessible and inclusive digital products and web experiences.',
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/images/dark/favicon.ico',
+        href: '/images/dark/favicon.ico',
+      },
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/images/light/favicon.ico',
+        href: '/images/light/favicon.ico',
+      },
+    ],
+  },
 }
